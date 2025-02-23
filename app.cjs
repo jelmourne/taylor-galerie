2dceeaf4304ffcebba421a8e041ccb9e0866aec3
Justin Elmourne <jelmourne@outlook.com>
2025-02-23 10:03:05 -0500

    CI/CD

--- app.cjs
+++ app.cjs
@@ -142,5 +142,5 @@ app.get("/*", async (req, res) => {
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${3000}`);
+  console.log(`Example app listening on port ${port}`);
 });
